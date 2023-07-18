9fe79affa7c86621b30fcd155648b46cd187e691
Faisal Mohammad <nirzon192@gmail.com>
2023-07-18 23:26:19 +0600

    set application

--- app/src/main/java/com/exam/singularity/AppApplication.kt
+++ app/src/main/java/com/exam/singularity/AppApplication.kt
@@ -1,4 +1,30 @@
 package com.exam.singularity
 
-class AppApplication {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppApplication : Application() {
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        // need to check internet from any where with out activityFragment
+        var headContext: Context? = null
+//        var isBackground = true
+//        var isLoggedIn = false
+//        var prefs: Prefs? = null
+
+//        @SuppressLint("StaticFieldLeak")
+//        lateinit var instance: MyApplication
+//            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        headContext = this.applicationContext
+//        instance = this
+//        prefs = Prefs(applicationContext)
+
+    }
 }
